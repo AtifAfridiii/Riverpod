c35fb95c5dce93325e4b9f77e3bfbf7f6c0a8da2
Atif Afridi <atifafridi378@gmail.com>
2024-10-06 22:57:20 +0500

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:state/home.dart';
 
+// declaring global data here :
 
 final nameProvider = Provider<String>((ref){
 return 'Atif';
